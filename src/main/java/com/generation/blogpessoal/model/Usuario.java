9f78eae6552dc71c94b5f7e88fed9e06bad0b35e
unknown <kvrah.eo.2013@gmail.com>
2023-09-12 12:16:40 -0300

    security

---
+++ src/main/java/com/generation/blogpessoal/model/Usuario.java
@@ -0,0 +1,93 @@
+package com.generation.blogpessoal.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name="tb_usuario")
+public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O nome é obrigatorio")
+	private String nome;
+	
+	@NotBlank(message="e-mail é obrigatorio")
+	@Email(message="Digite um e-mail válido")
+	private String usuario;
+	
+	@NotBlank(message="a senha é obrigatoria")
+	@Size(min=8,message="A senha deve ter no mínimo 8 caracteres")
+	private String senha;
+	
+	@Size(max=4000,message="O link da foto deve ter no máximo 4000 caracteres")
+	private String foto;
+	
+	@OneToMany(fetch=FetchType.LAZY,mappedBy="usuario",cascade=CascadeType.REMOVE)
+	@JsonIgnoreProperties("usuario")
+	private List <Postagem> postagem;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+}
